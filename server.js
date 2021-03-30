85a6133b25e5a8c0437bfd2bdb0f63a51634d2fe
Cheshan <www.cheshanj95@gmail.com>
2021-03-30 20:07:01 +0530

    google drive api

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const app = express();
+const multer = require('multer');
+const fs = require("fs");
+const Oauth2Data = require("./credentials.json");
 
 var cookieParser = require('cookie-parser')
+var authed = false;
 
 const { OAuth2Client } = require('google-auth-library');
-const CLIENT_ID = '555382673677-6t26jc8ug2lrrsofa80bmkrv3uo7ujgu.apps.googleusercontent.com';
-const client = new OAuth2Client(CLIENT_ID);
+const { google } = require('googleapis');
+const CLIENT_ID = Oauth2Data.web.client_id;
+const CLIENT_SECRET = Oauth2Data.web.client_secret
+const REDIRECT_URL = Oauth2Data.web.redirect_uris[0];
+const client = new google.auth.OAuth2(
+
+    CLIENT_ID, CLIENT_SECRET, REDIRECT_URL
+);
+
+const SCOPES =
+    "https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/userinfo.profile";
+
+var Storage = multer.diskStorage({
+
+    destination: function (req, res, callback) {
+        callback(null, "./images");
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    },
+
+});
+
+var upload = multer({
+    storage: Storage,
+}).single("file");
+
 
 //view engine middleware
 app.set('view engine', 'ejs');
@@ -16,7 +45,39 @@ app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
 
-    res.render('login');
+    if (!authed) {
+
+        var url = client.generateAuthUrl({
+
+            access_type: 'offline',
+            scope: SCOPES,
+        });
+        console.log(url);
+        res.render('login', { url: url });
+    } else {
+
+        var oauth2 = google.oauth2({
+            auth: client,
+            version: "v2"
+        });
+
+        oauth2.userinfo.get(function (err, response) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(response.data);
+                name = response.data.name
+                pic = response.data.picture
+                res.render("posts", {
+                    name: response.data.name,
+                    pic: response.data.picture,
+                    success: false
+                });
+            }
+        });
+    }
+
+
 });
 
 app.get('/home', checkAuthenticated, (req, res) => {
@@ -25,7 +86,70 @@ app.get('/home', checkAuthenticated, (req, res) => {
 
 });
 
-app.get('*', (req,res)=>{
+app.get('/posts', checkAuthenticated, (req, res) => {
+    let user = req.user;
+    res.render('posts', { user });
+
+});
+
+app.post("/upload", (req, res) => {
+    upload(req, res, function (err) {
+        if (err) {
+            console.log(err);
+            return res.end("File not uploaded. Please try again.");
+        } else {
+            console.log(req.file.path);
+            const drive = google.drive({ version: "v3", auth: client });
+            const fileMetadata = {
+                name: req.file.filename,
+            };
+            const media = {
+                mimeType: req.file.mimetype,
+                body: fs.createReadStream(req.file.path),
+            };
+            drive.files.create(
+                {
+                    resource: fileMetadata,
+                    media: media,
+                    fields: "id",
+                },
+                (err, file) => {
+                    if (err) {
+                        // Handle error
+                        console.error(err);
+                    } else {
+                        fs.unlinkSync(req.file.path)
+                        res.render("posts", { name: name, pic: pic, success: true })
+                    }
+
+                }
+            );
+        }
+    });
+});
+
+app.get("/google/callback", function (req, res) {
+    const code = req.query.code;
+    if (code) {
+        // Get an access token based on our OAuth code
+        client.getToken(code, function (err, tokens) {
+            if (err) {
+                console.log("Error authenticating");
+                console.log(err);
+            } else {
+                console.log("Successfully authenticated");
+                console.log(tokens)
+                client.setCredentials(tokens);
+
+
+                authed = true;
+                res.redirect("/");
+            }
+        });
+    }
+});
+
+app.get('*', (req, res) => {
 
     res.render('404');
 });
@@ -60,7 +184,7 @@ app.post('/login', (req, res) => {
 app.get('/logout', (req, res) => {
 
     res.clearCookie('session-token');
-    res.redirect('/');
+    res.redirect('/login');
 });
 
 function checkAuthenticated(req, res, next) {
